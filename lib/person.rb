8ef070ea4a5bfb955120a32c1f49314ba84f98f4
Laura Felce <crepu83@yahoo.it>
2018-10-08 15:12:09 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,18 @@
 class Person
   name= (person_name)
   @name = person_name
+end
   
-  def name@name
+  def name
+    @name
   end
+  
+  job= (person_job)
+  @job = person_job
+end
+
+def job
+  @job
+end
+
+end
